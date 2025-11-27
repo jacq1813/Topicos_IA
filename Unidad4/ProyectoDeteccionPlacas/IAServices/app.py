f546614f1a49e192bade8c2d415a4cab5bc4f97d
jacq1813 <l21170454@culiacan.tecnm.mx>
2025-11-27 16:28:39 -0700

    Prepare new deploy

--- Unidad4/ProyectoDeteccionPlacas/IAServices/app.py
+++ Unidad4/ProyectoDeteccionPlacas/IAServices/app.py
@@ -100,4 +100,4 @@ def analizar_placa():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=7860)
